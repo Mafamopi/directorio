e152ee060e22fd64916d4d59df9ee5a77096e34c
caliche <caliche@carlos>
2015-06-09 14:09:39 -0500

    Realise el cambio de checbox por un boton cree la clase contacpainter con sus metodos

--- src/directorio/view/ContactPainter.java
+++ src/directorio/view/ContactPainter.java
@@ -1,5 +1,14 @@
 package directorio.view;
 
+import directorio.cotroller.dto.ContactDTO;
+import java.util.List;
+import javax.swing.JPanel;
+
 public class ContactPainter {
     
+    public void paintContacts(JPanel panel,List<ContactDTO> contactList,String filtro){
+        for(ContactDTO contacts:contactList){
+            //TODO: crear logica para filtrar el nombre
+        }
+    }
 }
